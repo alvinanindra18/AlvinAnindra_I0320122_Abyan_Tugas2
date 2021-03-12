466724eb43727f607cf73bd08638355fd94a9b94
alvinanindra18 <80144813+alvinanindra18@users.noreply.github.com>
2021-03-12 14:54:38 +0700

    Tugas 2 Prokom Kelas C 2021

---
+++ I0320122_soal2_tugas2.py
@@ -0,0 +1,36 @@
+#identitas mahasiswa
+
+nama = " ALvin Anindra Putra"
+pekerjaan = "Mahasiswa"
+jurusan = "Teknik Industri"
+angkatan = 2020
+cita_cita = "direktur perusahaan multinasional"
+alamat = "Jl Merapi no 4, Banaran, Grogol, Sukoharjo"
+anak_ke = 2
+ibu = "Hany Handayani Warsita"
+ayah = "Tri Winarto"
+adik = "Amanda Dara Puspita"
+kakak = "Citra Mustika Putri"
+Umur = 18
+gol_darah = 'O'
+tinggi = 179
+berat_badan = 64
+ukuran_sepatu = 43
+hobi = "olahraga"
+alergi = "ayam dan keju"
+instagram = "@alvinanindra"
+
+print("Hai nama saya", nama, "umur saya", Umur)
+print("saya sekarang seorang", pekerjaan, "pada jurusan", jurusan, "angkatan", angkatan)
+print("cita-cita saya menjadi seorang", cita_cita)
+print("saya bertempat tinggal di", alamat)
+print("ayah saya adalah", ayah, "dan ibu saya adalah", ibu, "dan saya adalah anak ke", anak_ke)
+print(" saya memiliki adik yang bernama", adik, "dan seorang kaka bernama", kakak)
+print("golongan darah saya adalah", gol_darah)
+print("tinggi badan saya", tinggi, " dan berat badan saya", berat_badan)
+print("ukuran sepatu saya", ukuran_sepatu)
+print("hobi saya yaitu", hobi)
+print("akun instagram saya adalah", instagram)
+print("saya memiliki alergi terhadap", alergi)
+
+
